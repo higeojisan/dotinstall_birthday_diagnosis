1b5ecf2f35bec7a0a2aab37bbb8cd0ce987d97d5
m-kouda <makotokoda82@gmail.com>
2017-07-03 23:31:52 +0900

    誕生日の取得

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 (function() {
   'use strict';
+
+  var birthday = document.getElementById('birthday');
+  var calc_button = document.getElementById('calc');
+  calc_button.addEventListener('click', function() {
+      var birthday_value = birthday.value.split("-");
+      var year = birthday_value[0];
+      var month = birthday_value[1];
+      var day = birthday_value[2];
+  });
+
 })();
