dbd65c2b244746dd4317f34d18ef671f683cedd3
fl-kouda <makoto.kouda@firstlogic.co.jp>
2017-07-04 20:05:46 +0900

    経過日数と年齢の計算の実装

--- js/main.js
+++ js/main.js
@@ -3,11 +3,21 @@
 
   var birthday = document.getElementById('birthday');
   var calc_button = document.getElementById('calc');
-  calc_button.addEventListener('click', function() {
-      var birthday_value = birthday.value.split("-");
-      var year = birthday_value[0];
-      var month = birthday_value[1];
-      var day = birthday_value[2];
-  });
+  var result = document.getElementById('result');
 
+  calc_button.addEventListener('click', function() {
+      // 誕生日のUNIXタイムスタンプの取得
+      var birthday_date_obj = new Date(birthday.value);
+      var birthday_timestamp = birthday_date_obj.getTime();
+      // 現在時刻のUNIXタイムスタンプの取得
+      var now = Date.now();
+
+      // 経過日数の計算
+      var elapsedday = Math.floor((now - birthday_timestamp) / (1000 * 60 * 60 * 24));
+      // 年齢の計算
+      var age = Math.floor(elapsedday / 365.25);
+
+      // 表示
+      result.innerHTML = "<p>あなたは" + age + "歳で、生まれてから" + elapsedday + "日経過しています</p>";
+  });
 })();
